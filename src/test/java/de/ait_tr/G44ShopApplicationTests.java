178fbed011bf00b4b4401f2e39b93a4dd6cef06b
SergeyB <sergiibugaienko@gmail.com>
2024-08-16 13:08:36 +0200

    init commit

---
+++ src/test/java/de/ait_tr/G44ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait_tr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G44ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
