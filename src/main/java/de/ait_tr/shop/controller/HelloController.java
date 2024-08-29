56c0a26248462f725120d515a56b6ac6947e12de
SergeyB <sergiibugaienko@gmail.com>
2024-08-29 19:42:34 +0200

    Before deploy

---
+++ src/main/java/de/ait_tr/shop/controller/HelloController.java
@@ -0,0 +1,21 @@
+package de.ait_tr.shop.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 29.08.2024
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello(@Value("${messages.hello}") String helloMessage) {
+        return helloMessage;
+    }
+}
